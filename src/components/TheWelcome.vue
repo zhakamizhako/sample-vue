3f3c3e37175eff0c9fb134a9f80913cf1f9b4331
Roland Kim Andre Solon <roland@apollo.com.ph>
2022-10-19 17:57:54 +0800

    Initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    Hello World {{test}}
+  </div>
+  <div>
+    I am {{loggedIn ? "Logged in" : "Not logged in"}}
+  </div>
+  <div>
+    Use the native side of things to login.
+  </div>
+  <input :value="message" /><button @click="sendData">Send</button>
+</template>
+
+<script>
+var oWebViewInterface = null;
+
+export default {
+  name: "Homepage",
+  setup(){
+      
+      oWebViewInterface = window.nsWebViewInterface;
+      
+      if (oWebViewInterface) {
+        console.log("valid, emitting 'loaded'")
+        oWebViewInterface.emit("loaded", `initialized webview`);
+      }
+  },
+  methods:{
+    sendData(){
+      console.log(message)
+    }
+  },
+  data(){
+    return {
+      test: null,
+      message: null,
+      loggedIn: false,
+    }
+  },
+  methods(){
+
+  }
+}
+</script>
